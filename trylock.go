ba3e5da204fb49e4b8a6a7e430e9bed321d0caa5
twentydraft <clamores.pro@gmail.com>
2016-10-26 11:27:28 +0300

    trylock implementation

---
+++ trylock.go
@@ -0,0 +1,44 @@
+package trylock
+
+import (
+	"errors"
+	"runtime"
+	"sync/atomic"
+	"time"
+)
+
+var (
+	ErrUnlockOfUnlockedMutex = errors.New("unlock of unlocked mutex")
+)
+
+type TryMutex struct {
+	flag uint32
+}
+
+func (tryMutex *TryMutex) Lock() {
+	for !atomic.CompareAndSwapUint32(&tryMutex.flag, 0, 1) {
+		runtime.Gosched()
+	}
+}
+
+func (tryMutex *TryMutex) Unlock() {
+	if !atomic.CompareAndSwapUint32(&tryMutex.flag, 1, 0) {
+		panic(ErrUnlockOfUnlockedMutex)
+	}
+}
+
+func (tryMutex *TryMutex) TryLock(timeout time.Duration) (ok bool) {
+	timeoutFlag := uint32(0)
+	timeoutFlagPointer := &timeoutFlag
+	time.AfterFunc(timeout, func() {
+		atomic.CompareAndSwapUint32(timeoutFlagPointer, 0, 1)
+	})
+	for {
+		if atomic.CompareAndSwapUint32(timeoutFlagPointer, 1, 0) {
+			return false
+		} else if atomic.CompareAndSwapUint32(&tryMutex.flag, 0, 1) {
+			return true
+		}
+		runtime.Gosched()
+	}
+}
